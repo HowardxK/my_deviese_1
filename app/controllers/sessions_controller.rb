fb030d62c447c494ed29362ba780b0a70277929c
howard <howard67890@gmail.com>
2021-07-04 16:17:11 +0800

    Log in 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+  
+  def create
+    @user = User.find_by(email: params[:session][:email])
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to '/login', notice: '登入成功'
+    else
+      redirect_to '/login', notice: '登入失敗'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   
   resources :users
   root 'users#new'
-  get 'signup', to: 'users#new'
-  get 'login', to: 'sessions#new'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+ 
 end
