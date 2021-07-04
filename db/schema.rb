b73548b890bed6dc88cdc781242e4fbfab5e1f99
howard <howard67890@gmail.com>
2021-07-04 14:43:43 +0800

    Signup 2

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2021_07_04_052349) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password_digeset"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
